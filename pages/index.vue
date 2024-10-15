97f93455c9a5c1d9de45acdc3b6e1d77816f0bc7
MahyarBHZ <mahyarbhz@gmail.com>
2024-10-15 23:03:04 +0330

    Adding tailwind to project

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+    const { data: ping } = await useFetch('http://localhost:8000/api/ping')
+</script>
+
 <template>
     <div>
-        <NuxtRouteAnnouncer />
-        <NuxtWelcome />
+        Testing api:
+        We say ping!
+        They say:
+        {{ ping }}
     </div>
 </template>
